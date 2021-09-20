a441ea86fde87da82a123687762bf82a625d06a0
Yurii Ya <yurii.ya.dipp@gmail.com>
2021-09-20 15:37:50 +0800

    Added: search comp refactor to func

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,54 +1,49 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Radio} from './Radio'
 
-class Search extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			search: '',
-			categories: ['All', 'Movie', 'Series'],
-      filter: 'all'
-		}
-	}
+function Search(props) {
+	const {callback = Function.prototype} = props;
+	const [search, setSearch] = useState('');
+	const [filter, setFilter] = useState('all');
+	const [categories, setCategories] = useState(['All', 'Movie', 'Series']);
 
-	handleSearch = (event) => {
+	const handleSearch = (event) => {
 		if(event.key === 'Enter') {
-			this.props.callback(this.state.search, this.state.filter)
+			callback(search, filter);
 		} else {
-			this.setState({search: event.target.value});
+			setSearch(event.target.value);
 		}
 	}
 
-	handleFilter = (filter) => {
-		this.setState(() => ({filter: filter}), () => {this.props.callback(this.state.search, this.state.filter)});
+	const handleFilter = (filter) => {
+		setFilter(filter);
+		callback(search, filter);
 	}
 
-	render() {
-		return (
-			<>
-				<div className="input-field">
-				<input 
-					onKeyUp={this.handleSearch} 
-					onChange={this.handleSearch} 
-					placeholder="Seacrh" 
-					id="first_name" 
-					type="search" 
-					className="validate" 
-					value={this.state.search}
-				/>
-			</div>
-			<div className='categories'>{this.state.categories.map((category, id) => (
-				<Radio
-					handleFilter={this.handleFilter}
-					dataset={category.toLowerCase()} 
-					value={this.state.filter} 
-					key={id}>
-					{category}
-				</Radio>
-				))}</div>
-			</>
-		)
-	}
+	return (
+		<>
+			<div className="input-field">
+			<input 
+				onKeyUp={handleSearch} 
+				onChange={handleSearch} 
+				placeholder="Seacrh" 
+				id="first_name" 
+				type="search" 
+				className="validate" 
+				value={search}
+			/>
+		</div>
+		<div className='categories'>{categories.map((category, id) => (
+			<Radio
+				handleFilter={handleFilter}
+				dataset={category.toLowerCase()} 
+				value={filter} 
+				key={id}>
+				{category}
+			</Radio>
+			))}</div>
+		</>
+	)
 }
 
 export {Search}
