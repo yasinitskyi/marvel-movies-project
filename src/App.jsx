2c91f1a8f7c060f9d97a6f451fa0a8fffe1f3601
Yurii Ya <yurii.ya.dipp@gmail.com>
2021-09-20 16:11:18 +0800

    Added: refactor app to fucn component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { Header } from './layout/Header'
 import { Footer } from './layout/Footer'
 import { Main } from './layout/Main'
@@ -8,51 +8,47 @@ import { Search } from './components/Search'
 
 const API_KEY = process.env.REACT_APP_API_KEY;
 
-class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      movies: [],
-      loading: true
-    }
-  }
+function App() {
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  componentDidMount() {
+  useEffect(() => {
     fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=marvel`)
       .then(response => response.json())
-      .then(data => this.setState(() => ({movies: data.Search, loading: false}), () => console.log(this.state.movies)))
-      .catch(err => {
-        console.error(err);
-        this.setState({loading: false});
+      .then((data) => {
+        setMovies(data.Search);
+        setLoading(false);
+      }).catch(err => {
+        setLoading(false);
       })
-  }
+  }, []);
 
-  searchMovies = (str, type) => {
+  const searchMovies = (str, type) => {
     if(str.trim() !== '') {
-      this.setState({movies: [], loading: true})
+      setMovies([]);
+      setLoading(true);
       fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${str.toLowerCase()}${type !== 'all' ? `&type=${type}` : ''}`)
         .then(response => response.json())
-        .then(data => this.setState({movies: data.Search, loading: false}))
-        .catch(err => {
-          console.error(err);
-          this.setState({loading: false});
+        .then((data) => {
+          setMovies(data.Search);
+          setLoading(false);
+        }).catch(err => {
+          setLoading(false);
         })
     }
   }
 
-  render() {
-    const { movies, loading } = this.state;
-    return (
-      <>
-        <Header />
-        <Main>
-          <Search callback={this.searchMovies}/>
-          {loading ? (<Preloader />) : (<Movies movies={movies} />)}
-        </Main>
-        <Footer />
-      </>
-    );
-  }
+
+  return (
+    <>
+      <Header />
+      <Main>
+        <Search callback={searchMovies}/>
+        {loading ? (<Preloader />) : (<Movies movies={movies} />)}
+      </Main>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
